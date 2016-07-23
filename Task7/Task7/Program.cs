9f8b47263ee4896bc7c597f4a47fef3a59cd82c4
aminyaylo <minyailoandrey@gmail.com>
2016-07-23 15:09:48 +0300

    ff

--- Task7/Task7/Program.cs
+++ Task7/Task7/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace Task7
 {
-    class Program
+    class Treatment
     {
+        public string result(string someString)
+        {
+            ConsoleKeyInfo cki;
+            Console.WriteLine(someString);
+            cki = Console.ReadKey();
+            if ((cki.Modifiers & ConsoleModifiers.Control)
+        }
+    }
+    class Call
+    {
+        public static string input(string consoleMsg = "Enter somthing")
+        {
+            Console.WriteLine(consoleMsg);
+            var someInput = Console.ReadLine();
+            return someInput;
+        }
+
+        public static void result()
+        {
+            var someStrings = input();
+
+        }
+
         static void Main(string[] args)
         {
-            testRepo
+            result();
         }
     }
 }
---
+++ Task7/Test/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Test
+{
+    class PushKey
+    {
+        public  PushKey()
+        {
+        
+        ConsoleKeyInfo cki;
+        Console.TreatControlCAsInput = true;
+           Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
+            Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+            do
+            {
+                    cki = Console.ReadKey(true);
+                   
+                if ((cki.Modifiers & ConsoleModifiers.Control) != 0 & cki.Key == ConsoleKey.Z)
+                {
+                    Console.WriteLine("Congratulation! You Enter Ctrl and z");
+
+                }
+
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+    }
+
+    class Action
+    {
+        
+    }
+    class Program
+    {
+        public static string input(string consoleMsg = "Enter numder")
+        {
+            Console.WriteLine(consoleMsg);
+            var tmp = Console.ReadLine();
+            return tmp;
+        }
+        public static void consoleOut()
+        {
+            //Action ActionObject = new Action();
+            var consoleEnter = input("Enter something");
+
+        }
+        
+        public static void Main()
+        {
+           
+        }
+
+    }
+    }
+
